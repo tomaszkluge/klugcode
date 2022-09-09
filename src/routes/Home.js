e9d785ea94cf1d553fefad983c06d357e05b3e88
KlugSolutions <tomasz.kluge@gmail.com>
2022-09-09 14:29:36 +0200

    added video styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-
+import Video from '../components/Video'
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <Video />
     </div>
   )
 }
