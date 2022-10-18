899b8640348d659c1f804c4cf238f74a26c0e6ba
KlugSolutions <tomasz.kluge@gmail.com>
2022-10-18 19:00:33 +0200

    changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
     return (
         <div className='footer'>
             <div className='footer-container'>
-                <div className='email'>
+                <div className='left'>
                     <a href="mailto:info@klugcode.dev"><p><FaMailBulk className='ico' size={35} style={{ color: '#BB001B'}} />info@klugcode.dev</p></a>
                 </div>
-                <div className='social'>
+                <div className='right'>
                     <a href='https://www.facebook.com/KlugCodeSolutions' target='_blank' rel="noopener noreferrer"><p><FaFacebook className='ico' size={35} style={{ color: '#3b5998'}} />fb.com/KlugCodeSolutions</p></a>
                 </div>
             </div>
