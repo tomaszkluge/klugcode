e19e9556a7ac2f72d33da8d005b6d2b2ef4de674
KlugSolutions <tomasz.kluge@gmail.com>
2022-10-25 14:35:13 +0200

    changed index in public

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,15 +5,14 @@ import './FormStyles.css'
 const Form = () => {
   return (
       <div className='form'>
-        <form action="https://formsubmit.co/info@klugcode.dev" method='post'>
+        <form action="https://formsubmit.co/info@klugcode.dev" method='POST'>
           <label>Your Name</label>
           <input name='name' type='text' required></input>
           <label>Email</label>
           <input name='email' type='email' required></input>
-          <label>Subject</label>
-          <input type="text" name="subject" required></input>
+          <input type="hidden" name="_subject" value="New message from your site!"></input>
           <label>Details</label>
-          <textarea name='comment' rows='6' placeholder='Type a message_' required />
+          <textarea name='message' rows='6' placeholder='Type a message_' required />
           <button className='btn' type='submit'>Send</button>
         </form>
       </div>
